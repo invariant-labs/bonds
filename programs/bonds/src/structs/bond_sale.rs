d1e2ec7730fed1cb54769c1162c47396d7fe9f95
maciej-zieba <maciejzieba98@gmail.com>
2022-01-18 13:45:32 +0100

    Default for BondSale

--- programs/bonds/src/structs/bond_sale.rs
+++ programs/bonds/src/structs/bond_sale.rs
@@ -4,6 +4,7 @@ use super::{Decimal, TokenAmount};
 
 #[account(zero_copy)]
 #[repr(packed)]
+#[derive(Debug, Default)]
 pub struct BondSale {
     pub token_buy: Pubkey,
     pub token_sell: Pubkey,
@@ -18,26 +19,6 @@ pub struct BondSale {
     pub sell_amount: TokenAmount,
     pub sale_time: u64,
     pub nonce: u8,
-}
-
-impl Default for BondSale {
-    fn default() -> BondSale {
-        BondSale {
-            token_buy: Pubkey::default(),
-            token_sell: Pubkey::default(),
-            token_buy_account: Pubkey::default(),
-            token_sell_account: Pubkey::default(),
-            payer: Pubkey::default(),
-            floor_price: Decimal::default(),
-            up_bound: Decimal::default(),
-            velocity: Decimal::default(),
-            buy_amount: TokenAmount(0),
-            remaining_amount: TokenAmount(0),
-            sell_amount: TokenAmount(0),
-            sale_time: 0,
-            nonce: 0,
-        }
-    }
 }
 
 impl BondSale {
