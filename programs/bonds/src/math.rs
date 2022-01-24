0122e2cc99268df296afd943c6100c8f3cef204a
maciej-zieba <maciejzieba98@gmail.com>
2022-01-24 16:14:00 +0100

    add tests to calculate_new_price

--- programs/bonds/src/math.rs
+++ programs/bonds/src/math.rs
@@ -15,7 +15,7 @@ pub fn calculate_new_price(
         / Decimal::from_integer(bond_sale.sale_time.try_into().unwrap());
 
     let delta_price: Decimal = bond_sale.velocity * bond_sale.up_bound * time_ratio;
-    let supply_ratio: Decimal = buy_amount.percent(bond_sale.sell_amount);
+    let supply_ratio: Decimal = buy_amount.percent(bond_sale.buy_amount);
 
     let price: Decimal = match bond_sale.previous_price < bond_sale.floor_price + delta_price {
         true => bond_sale.floor_price,
@@ -26,4 +26,60 @@ pub fn calculate_new_price(
     bond_sale.previous_price = price + jump;
 
     price + Decimal::from_decimal(50, 2) * bond_sale.up_bound
+}
+
+#[cfg(test)]
+mod tests {
+
+    use std::default;
+
+    use super::*;
+
+    #[test]
+    #[should_panic]
+    fn test_panic_calculate_new_price() {
+        let nonpanic_bond_sale = BondSale {
+            previous_price: Decimal::from_integer(2),
+            up_bound: Decimal::from_decimal(50, 2),
+            velocity: Decimal::one(),
+            buy_amount: TokenAmount(10),
+            remaining_amount: TokenAmount(20),
+            sell_amount: TokenAmount(30),
+            sale_time: 604800, // second in a week
+            last_trade: 0,
+            ..Default::default()
+        };
+
+        // supply cannot be equal to 0
+        {
+            let mut bond_sale = BondSale {
+                buy_amount: TokenAmount(0),
+                ..nonpanic_bond_sale
+            };
+            calculate_new_price(&mut bond_sale, 20, TokenAmount(2));
+        }
+        // velocity should be grater than 0
+        {
+            let mut bond_sale = BondSale {
+                velocity: Decimal::new(0),
+                ..nonpanic_bond_sale
+            };
+            calculate_new_price(&mut bond_sale, 20, TokenAmount(2));
+        }
+        // delta_time cannot be greater than sale_time
+        {
+            let mut bond_sale = BondSale {
+                last_trade: 302400,
+                ..nonpanic_bond_sale
+            };
+            calculate_new_price(&mut bond_sale, 302401, TokenAmount(2));
+        }
+        // buy_amount cannot be greater than remaining_amount
+        {
+            let mut bond_sale = BondSale {
+                ..nonpanic_bond_sale
+            };
+            calculate_new_price(&mut bond_sale, 20, TokenAmount(20));
+        }
+    }
 }
