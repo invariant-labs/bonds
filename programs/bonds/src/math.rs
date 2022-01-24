2bf63efa459466c4241a65a97f78c3242cae4b59
maciej-zieba <maciejzieba98@gmail.com>
2022-01-24 15:29:49 +0100

    add calculate_new_price

--- programs/bonds/src/math.rs
+++ programs/bonds/src/math.rs
@@ -6,31 +6,24 @@ use crate::{
 };
 
 pub fn calculate_new_price(
-    bond_sale: BondSale,
-    bond: &mut Bond,
+    bond_sale: &mut BondSale,
+    current_time: u64,
     buy_amount: TokenAmount,
 ) -> Decimal {
-    let fraction = bond_sale.velocity.big_div(Decimal::from_integer(
-        bond_sale.sale_time.try_into().unwrap(),
-    ));
-    let time_diff = get_current_timestamp()
-        .checked_sub(bond.last_trade)
-        .unwrap();
-    let price_increment =
-        buy_amount.percent(bond_sale.buy_amount) * bond_sale.up_bound * bond_sale.floor_price;
-    let slope = fraction * Decimal::from_integer(time_diff.try_into().unwrap());
+    let delta_time: u64 = current_time - bond_sale.last_trade;
+    let time_ratio: Decimal = Decimal::from_integer(delta_time.try_into().unwrap())
+        / Decimal::from_integer(bond_sale.sale_time.try_into().unwrap());
 
-    let temp = bond.current_price;
-    let price;
-    if slope.v.gt(&(price_increment + bond.previous_price).v) {
-        price = bond_sale.floor_price;
-    } else {
-        price = std::cmp::max(
-            bond_sale.floor_price,
-            bond.previous_price + price_increment - slope,
-        )
-    }
-    bond.update_previous_price(temp);
+    let delta_price: Decimal = bond_sale.velocity * bond_sale.up_bound * time_ratio;
+    let supply_ratio: Decimal = buy_amount.percent(bond_sale.sell_amount);
 
-    price
+    let price: Decimal = match bond_sale.previous_price < bond_sale.floor_price + delta_price {
+        true => bond_sale.floor_price,
+        false => bond_sale.previous_price - delta_price,
+    };
+    let jump: Decimal = supply_ratio * bond_sale.up_bound * bond_sale.floor_price;
+
+    bond_sale.previous_price = price + jump;
+
+    price + Decimal::from_decimal(50, 2) * bond_sale.up_bound
 }
