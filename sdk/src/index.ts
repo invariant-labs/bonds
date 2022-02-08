8b8b3fd3cd57a6ed36ef0b00f5a5d50ce427f637
K-Pomian <pomian.konrad@gmail.com>
2022-02-08 16:12:02 +0100

    Trunk

--- sdk/src/index.ts
+++ sdk/src/index.ts
@@ -1,9 +1,7 @@
-import { Bonds } from './sale'
-
 import { PublicKey, Transaction } from '@solana/web3.js'
 import { getProgramAddress, Network } from './network'
 
-export { Bonds, Network, getProgramAddress }
+export { Network, getProgramAddress }
 export interface IWallet {
   signTransaction: (tx: Transaction) => Promise<Transaction>
   signAllTransactions: (txs: Transaction[]) => Promise<Transaction[]>
